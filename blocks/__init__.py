d3d45efeb37f89271b7a19695e5f5daddf21eb40
Markus Heckner <mheckner@users.noreply.github.com>
2026-08-07 17:01:54 +0200

    Match check names to the original conftest.py emoji labels

--- blocks/__init__.py
+++ blocks/__init__.py
@@ -3,43 +3,43 @@ import check50
 
 @check50.check()
 def exists():
-    """blocks.py exists"""
+    """📁\tDie Datei blocks.py existiert"""
     check50.exists("blocks.py")
 
 
 @check50.check(exists)
 def rejects_negative():
-    """rejects a negative size"""
+    """🙅\tProgramm akzeptiert keine negativen Werte"""
     check50.run("python3 blocks.py").stdin("-1", prompt=False).stdin("1", prompt=False).stdout("#", regex=False)
 
 
 @check50.check(exists)
 def rejects_zero():
-    """rejects a size of 0"""
+    """🙅\tProgramm akzeptiert den Wert 0 nicht"""
     check50.run("python3 blocks.py").stdin("0", prompt=False).stdin("1", prompt=False).stdout("#", regex=False)
 
 
 @check50.check(exists)
 def rejects_nine():
-    """rejects a size of 9"""
+    """🙅\tProgramm akzeptiert den Wert 9 nicht"""
     check50.run("python3 blocks.py").stdin("9", prompt=False).stdin("1", prompt=False).stdout("#", regex=False)
 
 
 @check50.check(exists)
 def size_one():
-    """generates a correct wall for size 1"""
+    """🧱\tMauer für den Wert 1 korrekt ausgegeben"""
     check50.run("python3 blocks.py").stdin("1", prompt=False).stdout("#", regex=False)
 
 
 @check50.check(exists)
 def size_two():
-    """generates a correct wall for size 2"""
+    """🧱\tMauer für den Wert 2 korrekt ausgegeben"""
     check50.run("python3 blocks.py").stdin("2", prompt=False).stdout("#*\n*#", regex=False)
 
 
 @check50.check(exists)
 def size_eight():
-    """generates a correct wall for size 8"""
+    """🧱\tMauer für den Wert 8 korrekt ausgegeben"""
     check50.run("python3 blocks.py").stdin("8", prompt=False).stdout(
         "#*#*#*#*\n*#*#*#*#\n#*#*#*#*\n*#*#*#*#\n#*#*#*#*\n*#*#*#*#\n#*#*#*#*\n*#*#*#*#",
         regex=False,
